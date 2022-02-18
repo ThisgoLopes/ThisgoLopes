e91806b0876132b70a1648e59b17472e8ac870b0
Thiago Lopes de Olivera <92656412+ThisgoLopes@users.noreply.github.com>
2022-02-17 21:36:14 -0300

    Update tarefa_2_struct.c

--- tarefa_2_struct.c
+++ tarefa_2_struct.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define n_pessoas 2
+#define n_pessoas 2 //Utilizei a mesma ideia de definir um numero menor para testes
 #define max_nome 30
 
 int main (){
@@ -15,33 +15,35 @@ int main (){
     typedef struct {
         char nome[max_nome];
         int cpf;
-        tp_data; 
+        tp_data nascimento; 
     } tp_dados;
 
-    tp_dados info[n_pessoas]
+    tp_dados info[n_pessoas];
 
     for (i=0; i<n_pessoas; i++){
-        fflush(stdin);
         printf("\nInforme o nome: ");
         setbuf(stdin, NULL);
-        fgets(dados_pp[i].nome, max_nome, stdin);
+        fgets(info[i].nome, max_nome, stdin);
+        fflush(stdin);
         printf("Qual seu CPF: ");
-        scanf("%d", &dados_pp[i].cpf);
+        scanf("%d", &info[i].cpf);
         printf("Digite o dia: ");
-        scanf("%d", &n_data[i].dia);
+        scanf("%d", &info[i].nascimento.dia);
         printf("Digite o mes: ");
-        scanf("%d", &n_data[i].mes);
+        scanf("%d", &info[i].nascimento.mes);
         printf("Digite o ano: ");
-        scanf("%d", &n_data[i].ano);
+        scanf("%d", &info[i].nascimento.ano);
     }
-    
-    for(i=0;i<=n_pessoas; i++){
+    for(i=0;i<=n_pessoas; i++)
+    {
         printf("\n-----Informacoes-----\n");
         printf("Nome: %s", info[i].nome);
         printf("CPF: %d \n", info[i].cpf);
         printf("Nascimento: %d/%d/%d \n", info[i].nascimento.dia, info[i].nascimento.mes, info[i].nascimento.ano);
     }
-      /* Esta dando um erro na hora de mostrar os dados, ela mostra 1 dado a mais do que foi pedido com os campos de informações 
+
+    /* Esta dando um erro na hora de mostrar os dados, ela mostra 1 dado a mais do que foi pedido com os campos de informações 
     sem preencher ou com numeros aleatórios*/
+
     return 0;
 }
